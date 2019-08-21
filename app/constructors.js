2f5d55d15ff4dd2d06d56d7373e7c3e5b4dfa6a1
horhik <horh1khcj@gmail.com>
2019-08-21 13:16:33 -0400

    rewrite on p5js

--- app/constructors.js
+++ app/constructors.js
@@ -1,10 +1,11 @@
-const canvas = document.querySelector('.snake')
-canvas.width = innerWidth
-canvas.height = innerHeight
-const c = canvas.getContext('2d')
-const randomX = Math.random() * canvas.width
-const randomY = Math.random() * canvas.height
+// const canvas = document.querySelector('.snake')
+// canvas.width = innerWidth
+// canvas.height = innerHeight
+// const c = canvas.getContext('2d')
+// const randomX = Math.random() * canvas.width
+// const randomY = Math.random() * canvas.height
 // eslint-disable-next-line no-unused-vars
+
 class Snake {
     constructor (len, speed) {
         this.x = window.innerWidth / 2
@@ -21,87 +22,51 @@ class Snake {
         this.speed = speed
         this.speedX = 0
         this.speedY = 0
-        this.img = new Image(100, 100)
-        this.img.src = 'sprites.png'
-        this.texture = {
-            small: {
-                head: [this.img, 208, 62, 18, 30],
-                body: [this.img, 208, 112, 18, 30],
-                tile: [this.img, 160, 120, 18, 30]
-            },
-            middle: {
-                head: '../src/img/middle-head',
-                body: '../src/img/middle-body',
-                tile: '../src/img/middle-tile'
-            },
-            big: {
-                head: '../src/img/big-head',
-                body: '../src/img/big-body',
-                tile: '../src/img/big-tile'
-            }
+        this.update = () => {
+            this.scaling()
+            this.teleport()
+            this.tiling()
+
         }
-        this.render = () => {
-            this.color++
+        this.show = () => {
+            fill(`hsl(${this.color}, 50%, 50%)`)
+            rect(this.x, this.y, this.sizeX, this.sizeY);
+          this.tiling()
+          console.log(this.color)
+          for(let i = 0; i < this.tile.length; i++){
+            fill(`hsl(${this.color}, 50%, 50%)`)
+            rect(this.tile[i].x, this.tile[i].y, this.sizeX, this.sizeY)
+          }
+            noStroke()
+        }
+        this.teleport = () => {
+          if (this.x + this.sizeX < 0 ){
+            this.x = innerWidth
+          }
+          if (this.x > innerWidth){
+            this.x =  0
+          }
+          if (this.y + this.sizeY < 0 ){
+            this.y = innerHeight
+          }
+          if (this.y > innerHeight){
+            this.y = 0
+          }
+        }
+        this.scaling = () => {
+            this.speed *= 1.00004
             this.x += this.speedX
             this.y += this.speedY
-            c.fillStyle = `hsl(${this.color}, 50%, 50%)`
-            for (var i = 0; i < this.tile.length; i++) {
-                // if (i === 0) {
-                // c.drawImage(...this.texture.small.tile, this.tile[i].x, this.tile[i].y + 60, this.sizeX, this.sizeY)
-                // } else if (i === this.tile.length - 1) {
-                //   c.drawImage(...this.texture.small.head, this.tile[i].x, this.tile[i].y, this.sizeX, this.sizeY)
-                // } else {
-                // c.rotate(Math.PI)
-                // c.drawImage(...this.texture.small.body, this.tile[i].x, this.tile[i].y + 25, this.sizeX, this.sizeY)
-                c.fillStyle = `hsl(${this.color}, 50%, 50%)`
-                c.fillRect(this.tile[i].x, this.tile[i].y, this.sizeX, this.sizeY)
-
-                this.showHide()
-            // }
-        }
-            this.tile.push({ x: this.x, y: this.y })
-            if (this.tile.length > this.length) {
-                this.tile.shift()
+        this.color++
+            if (this.color > 360){
+                this.color = 0
             }
         }
-        this.showHide = (x, y) => {
-            // checking left border
-            if (this.x > canvas.width) {
-                this.x = 0 - this.speedX
-            }
-            // checking right border
-            if (this.x + this.sizeX < 0) {
-                this.x = canvas.width - this.sizeX - this.speedX
-            }
-            // checking bottom border
-            if (this.y > canvas.height) {
-                this.y = -this.speedY
-            }
-            // checking top border
-            if (this.y + this.sizeY < 0) {
-                console.log(this.y)
-                this.y = canvas.height - this.sizeY
-            }
+        this.tiling = () =>  {
+          this.tile.push({x: this.x, y: this.y})
+          if (this.tile.length > this.length){
+            this.tile.shift()
+          }
         }
-        c.fillStyle = `hsl(${this.color}, 50%, 50%)`
-        c.fillRect(this.x, this.y, this.sizeX, this.sizeY)
-    }
+      }
 }
-const Feed = function (type, kind = false) {
-    this.temper = type
-    this.kind = kind
-    if (this.temper === true) {
-        if (kind === 'simple') {
-            this.color = 'green'
-            this.baff = 1
-            this.radius = 15
-        } else if (kind === 'taste') this.baff = 5
-    }
-    this.render = () => {
-        function draw () {
-            fill(90)
-            ellipse(200, 200, this.radius, this.radius)
-        }
-    }
-}
-// -- == === =!= != -> >= <= () {} {||} [||] && || {} [] *** ** * 🃔  ♂
--- app/index.js
+++ app/index.js
@@ -1,41 +1,30 @@
-/* eslint-disable no-undef */
-const snake = new Snake(10, 8)
-// eslint-disable-next-line no-undef
-$('keyDown', function () {
-    if (!(snake.speedY < 0)) {
-    snake.speedY = snake.speed
-    snake.speedX = 0
-    }
-})
-$('keyUp', function () {
-    if (!(snake.speedY > 0)) {
-        snake.speedY = -snake.speed
-        snake.speedX = 0
-    }
-})
-$('keyRight', function () {
-    if (!(snake.speedX < 0)) {
-    snake.speedX = snake.speed
-    snake.speedY = 0
-    }
-})
-$('keyLeft', function () {
-    if (!(snake.speedX > 0)) {
-    snake.speedX = -snake.speed
-    snake.speedY = 0
-    }
-})
+let snake
+function setup () {
+    createCanvas(window.innerWidth, window.innerHeight)
+    snake = new Snake(30, 6)
+}
 
-setInterval(() => {
-    // var previousX = snake.x
-    // var previousY = snake.y
-    // snake.x += snake.speedX
-    // snake.y += snake.speedy
-    c.clearRect(0, 0, canvas.clientWidth, canvas.clientHeight)
-    snake.color += 0.02
-    snake.render()
-    for (let i = 0; i < 5; i++) {
-        const feed = new Feed(true, 'simple')
-        feed.render()
+function draw () {
+  background(53)
+  snake.update()
+  snake.show()
+}
+
+function keyPressed () {
+    if (keyCode === LEFT_ARROW && snake.speedX != snake.speed) {
+            snake.speedX = -snake.speed
+            snake.speedY = 0
     }
-}, 1000 / 60)
+    if (keyCode === RIGHT_ARROW && snake.speedX != -snake.speed) {
+            snake.speedX = snake.speed
+            snake.speedY = 0
+    }
+    if (keyCode === UP_ARROW && snake.speedY != snake.speed) {
+            snake.speedY = -snake.speed
+            snake.speedX = 0
+    }
+    if (keyCode === DOWN_ARROW && snake.speedY != -snake.speed) {
+            snake.speedY = snake.speed
+            snake.speedX = 0
+    }
+}
