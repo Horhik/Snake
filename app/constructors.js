e6f952919faced58ca03c942d4c161cbc7b61e1d
horhik <horh1khcj@gmail.com>
2019-08-22 10:07:45 -0400

    snake can eating! v 0.3.1

--- app/constructors.js
+++ app/constructors.js
@@ -1,72 +1,143 @@
-// const canvas = document.querySelector('.snake')
-// canvas.width = innerWidth
-// canvas.height = innerHeight
-// const c = canvas.getContext('2d')
-// const randomX = Math.random() * canvas.width
-// const randomY = Math.random() * canvas.height
-// eslint-disable-next-line no-unused-vars
+class Game {
+  	constructor () {
+		  this.hasFeed = false
+  	}
+	created(){}
+}
 
 class Snake {
-    constructor (len, speed) {
-        this.x = window.innerWidth / 2
-        this.y = window.innerHeight / 2
-        this.head = {
-            x: window.innerWidth / 2,
-            y: window.innerHeight / 2
-        }
-        this.sizeX = 20 // with textures 55.5
-        this.sizeY = 20 // with textures 62.5
-        this.color = 0
-        this.tile = []
-        this.length = len
-        this.speed = speed
-        this.speedX = 0
-        this.speedY = 0
-        this.update = () => {
-            this.scaling()
-            this.teleport()
-            this.tiling()
-
-        }
-        this.show = () => {
-            fill(`hsl(${this.color}, 50%, 50%)`)
-            rect(this.x, this.y, this.sizeX, this.sizeY);
-          this.tiling()
-          console.log(this.color)
-          for(let i = 0; i < this.tile.length; i++){
-            fill(`hsl(${this.color}, 50%, 50%)`)
-            rect(this.tile[i].x, this.tile[i].y, this.sizeX, this.sizeY)
-          }
-            noStroke()
-        }
-        this.teleport = () => {
-          if (this.x + this.sizeX < 0 ){
-            this.x = innerWidth
-          }
-          if (this.x > innerWidth){
-            this.x =  0
-          }
-          if (this.y + this.sizeY < 0 ){
-            this.y = innerHeight
-          }
-          if (this.y > innerHeight){
-            this.y = 0
-          }
-        }
-        this.scaling = () => {
-            this.speed *= 1.00004
-            this.x += this.speedX
-            this.y += this.speedY
-        this.color++
-            if (this.color > 360){
-                this.color = 0
-            }
-        }
-        this.tiling = () =>  {
-          this.tile.push({x: this.x, y: this.y})
-          if (this.tile.length > this.length){
-            this.tile.shift()
-          }
-        }
+  constructor (len, speed) {
+    this.x = window.innerWidth / 2
+    this.y = window.innerHeight / 2
+    this.head = {
+      x: window.innerWidth / 2,
+      y: window.innerHeight / 2
+    }
+    this.sizeX = 20 // with textures 55.5
+    this.sizeY = 20 // with textures 62.5
+    this.color = 0
+    this.tile = []
+	this.score = 0
+    this.length = len
+    this.speed = speed
+    this.speedX = 0
+    this.speedY = 0
+  }
+    update(){
+      this.scaling()
+      this.teleport()
+      this.tiling()
+    }
+    show(){
+      fill(`hsl(${this.color}, 50%, 50%)`)
+      rect(this.x, this.y, this.sizeX, this.sizeY)
+      this.tiling()
+      for (let i = 0; i < this.tile.length; i++) {
+        fill(`hsl(${this.color}, 50%, 50%)`)
+        rect(this.tile[i].x, this.tile[i].y, this.sizeX, this.sizeY)
       }
+      noStroke()
+    }
+    teleport(){
+      if (this.x + this.sizeX < 0) {
+        this.x = innerWidth
+      }
+      if (this.x > innerWidth) {
+        this.x = 0
+      }
+      if (this.y + this.sizeY < 0) {
+        this.y = innerHeight
+      }
+      if (this.y > innerHeight) {
+        this.y = 0
+      }
+    }
+    scaling(){
+      this.speed *= 1.00004
+      this.x += this.speedX
+      this.y += this.speedY
+      this.color++
+      if (this.color > 360) {
+        this.color = 0
+      }
+    }
+    tiling(){
+      this.tile.push({ x: this.x, y: this.y })
+      if (this.tile.length > this.length) {
+        this.tile.shift()
+      }
+    }
+	encounter(eat, place = game){
+		if ((eat.x + eat.size) <= this.x + this.sizeX && (eat.x) >= this.x || (eat.y + eat.size) <= (this.y + this.sizeY) && eat.y >- this.y ){
+			place.hasFeed = false
+			console.log(place)
+			this.score++
+			document.querySelector('.score__number').textContent = this.score
+		}
+	}
+}
+class Feed {
+  	constructor (game, size = 30, rare = 'simple', temper = true) {
+	  	this.rare = rare
+	  	this.x = Math.random() * width
+	  	this.y = Math.random() * height
+		this.color = 90
+		this.rainbow = `hsl(${color}, 40%, 70%)`
+		this.real =[]
+	  	this.sizeX = size
+	  	this.sizeY = size
+	  	this.temper = temper  
+	  	this.sts = 					// from "salutariness"
+	  		rare === "simple" ? 1 :
+			rare === "middle" ? 3 :
+			rare === "rare" ? 5 : 1
+
+}
+  	animation(){
+		  this.color = this.color > 360 ? 0 : this.color + 5
+		  if (this.real[3]) {this.real[3] = `hsl(${this.color}, 95%, 90%)`}
+		  this.rainbow = `hsl(${this.color}, 40%, 70%)`
+	}
+	show(){
+
+		this.real = []
+		this.animation()
+	    let eatRandom = ~~(Math.random() * 100)
+		this.x = ~~(Math.random() * width)
+		this.y = ~~(Math.random() * height)
+	    if (eatRandom <= 60){
+		    this.rare = 'simple'
+		    this.size = 30
+			fill('red')
+			this.real[3] = 'red'
+			ellipse(this.x, this.y, this.size)
+			noStroke()
+		}
+		else if (eatRandom <= 55){
+		    this.rare = 'middle'
+		    this.size = 20
+			fill('yellow')
+			this.real[3] = 'yellow'
+			ellipse(this.x, this.y, this.size)
+			noStroke()
+		}
+		else if (eatRandom <= 100){
+		    this.rare = 'rare'
+		    this.size = 10
+			this.real[3] = this.color
+			fill(`hsl(${this.real[3]}, 30%, 50%)`)
+			ellipse(this.x, this.y, this.size)
+			noStroke()
+		}
+		this.real[0] = this.x
+		this.real[1] = this.y
+		this.real[2] = this.size
+        game.hasFeed  = true
+	}
+	update(){
+		fill(this.real[3])
+		this.animation()
+		ellipse(this.real[0], this.real[1], this.real[2])
+		noStroke()	
+	}
 }
--- app/index.js
+++ app/index.js
@@ -1,30 +1,38 @@
-let snake
+
+let snake = ''
+let game = new Game()
+let feed
 function setup () {
     createCanvas(window.innerWidth, window.innerHeight)
-    snake = new Snake(30, 6)
-}
+    snake = new Snake(100, 6, game)
+        feed = new Feed(game)
 
-function draw () {
-  background(53)
-  snake.update()
-  snake.show()
 }
 
 function keyPressed () {
-    if (keyCode === LEFT_ARROW && snake.speedX != snake.speed) {
+    if (keyCode === LEFT_ARROW && snake.speedX === 0) {
             snake.speedX = -snake.speed
             snake.speedY = 0
     }
-    if (keyCode === RIGHT_ARROW && snake.speedX != -snake.speed) {
+    if (keyCode === RIGHT_ARROW && snake.speedX === 0) {
             snake.speedX = snake.speed
             snake.speedY = 0
     }
-    if (keyCode === UP_ARROW && snake.speedY != snake.speed) {
+    if (keyCode === UP_ARROW && snake.speedY === 0) {
             snake.speedY = -snake.speed
             snake.speedX = 0
     }
-    if (keyCode === DOWN_ARROW && snake.speedY != -snake.speed) {
+    if (keyCode === DOWN_ARROW && snake.speedY === 0) {
             snake.speedY = snake.speed
             snake.speedX = 0
     }
+}
+function draw () { 
+        background(53)
+        if(!game.hasFeed){feed.show()}
+        feed.update()
+        snake.encounter(feed, game)
+        snake.update()
+        snake.show()
+
 }
